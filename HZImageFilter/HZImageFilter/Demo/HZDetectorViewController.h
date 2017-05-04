7b4b9daabe4b12aba9497e20925965a8032b8cbb
zzgocc <zzgocc@gmail.com>
2017-05-04 16:14:22 +0800

    +add detector

---
+++ HZImageFilter/HZImageFilter/Demo/HZDetectorViewController.h
@@ -0,0 +1,13 @@
+//
+//  HZDetectorViewController.h
+//  HZImageFilter
+//
+//  Created by zz go on 2017/5/4.
+//  Copyright © 2017年 zzgo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HZDetectorViewController : UIViewController    
+
+@end
---
+++ HZImageFilter/HZImageFilter/Demo/HZDetectorViewController.m
@@ -0,0 +1,73 @@
+//
+//  HZDetectorViewController.m
+//  HZImageFilter
+//
+//  Created by zz go on 2017/5/4.
+//  Copyright © 2017年 zzgo. All rights reserved.
+//
+
+#import "HZDetectorViewController.h"
+#import <CoreImage/CoreImage.h>
+
+@interface HZDetectorViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *oldImageView;
+
+@end
+
+@implementation HZDetectorViewController
+
+
+- (IBAction)tapSubmitButton:(id)sender {
+    
+    
+    CIDetector *detector = [CIDetector detectorOfType:CIDetectorTypeFace
+                                              context:nil
+                                              options:nil];
+    
+    CIImage *image=[[CIImage alloc] initWithImage:self.oldImageView.image];
+    NSArray *faceArray = [detector featuresInImage:image
+                                           options:nil];
+    // Create a green circle to cover the rects that are returned.
+    
+//    CIImage *maskImage = nil;
+       CIImage *maskImage = [[CIImage alloc] initWithImage:self.oldImageView.image];
+    
+    for (CIFeature *f in faceArray) {
+        CGFloat centerX = f.bounds.origin.x + f.bounds.size.width / 2.0;
+        CGFloat centerY = f.bounds.origin.y + f.bounds.size.height / 2.0;
+        CGFloat radius = MIN(f.bounds.size.width, f.bounds.size.height) / 1.5;
+        CIFilter *radialGradient = [CIFilter filterWithName:@"CIRadialGradient" 
+                                        withInputParameters:@{
+                                                              @"inputRadius0": @(radius),
+                                                              @"inputRadius1": @(radius + 1.0f),
+                                                              @"inputColor0": [CIColor colorWithRed:0.0 green:1.0 blue:0.0 alpha:1.0],
+                                                              @"inputColor1": [CIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.0],
+                                                              kCIInputCenterKey: [CIVector vectorWithX:centerX Y:centerY],
+                                                              }];
+        CIImage *circleImage = [radialGradient valueForKey:kCIOutputImageKey];
+        
+        if (nil == maskImage)
+            maskImage = circleImage;
+        else
+            maskImage = [[CIFilter filterWithName:@"CISourceOverCompositing" 
+                              withInputParameters:@{
+                                                    kCIInputImageKey: circleImage,
+                                                    kCIInputBackgroundImageKey: maskImage,
+                                                    }] 
+                         valueForKey:kCIOutputImageKey];
+    }
+    
+    
+    //组合
+//    UIImage *oldImage=[self.oldImageView.image copy];
+//    
+//    CIImage *resulImage = [[CIFilter filterWithName:@"CISourceOverCompositing" 
+//                                      keysAndValues:kCIInputImageKey,maskImage,kCIInputBackgroundImageKey,oldImage,nil] 
+//                           valueForKey:kCIOutputImageKey];
+    
+    
+    //更新
+    self.oldImageView.image=[UIImage imageWithCIImage:maskImage];
+}
+
+@end
